4f10023964e7e2e5b93d0b11f4e078b950b69006
odedbe24-meet <106547160+odedbe24-meet@users.noreply.github.com>
2023-07-20 18:11:52 +0300

    done

--- Login-Session-Lab/login_session.py
+++ Login-Session-Lab/login_session.py
@@ -4,11 +4,21 @@ from flask import session as login_session
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-@app.route('/', ) # What methods are needed?
+@app.route('/', methods=['POST','GET']) # What methods are needed?
 def home():
-	
-	return render_template('home.html')
+	if request.method == "GET":
+		return render_template('home.html')
+	else:
+		try:
+			login_session['age'] = request.form.get('age',False)
+			login_session['name'] = request.form.get('name',False)
+			login_session['quote'] = request.form.get('quote',False)
+			return render_template('thanks.html')
+		except:
+			return render_template('error.html')
 
+
+	
 
 @app.route('/error')
 def error():
@@ -18,14 +28,19 @@ def error():
 
 @app.route('/display')
 def display():
+	age = login_session['age']
+	name = login_session['name']
+	quote = login_session['quote']
 
-	return render_template('display.html', ) # What variables are needed?
+
+
+	return render_template('display.html',name=name, age=age,quote=quote ) # What variables are needed?
 
 
 @app.route('/thanks')
 def thanks():
-
-	return render_template('thanks.html')
+	
+	return render_template('thanks.html' )
 
 
 if __name__ == '__main__':
